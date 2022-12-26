8fbf1acaf34d9c8bb82a4b5271d801ba7f7c3ff0
YahyaAvesta7 <yahyaraaedf7@gmail.com>
2022-12-26 13:31:16 +0300

    step-9

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,4 @@
-﻿import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:taosil/constants/routes.dart';
 import '../utilities/show_error_dialog.dart';
